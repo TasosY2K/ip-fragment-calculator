64bc8031650785e798b7284fe2bed3208c248e07
TasosY2K <gustavolama20@gmail.com>
2020-05-04 21:12:31 +0300

    fix: small code cleanup

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -6,7 +6,6 @@ new Vue({
       data_size: null,
       mtu: null,
       header_length: null,
-      fragments: [],
     };
   },
   methods: {
@@ -15,21 +14,10 @@ new Vue({
       const payloadOffsetBytes = payloadOffsetOctets * 8;
       const fragmentNum = parseInt((this.data_size / payloadOffsetBytes) + 1);
 
-      const fragments = [];
       let tableRows = '';
 
       let count = fragmentNum;
       for (let i = 0; i < fragmentNum; i++) {
-        fragments.push({
-          check: 'check',
-          df: 0,
-          mf: 1,
-          size: this.mtu - this.header_length,
-          header: this.header_length * 4,
-          final_size: this.mtu,
-          pointer: i * payloadOffsetOctets,
-        });
-
         count = count - 1;
         if (count == 0) {
           tableRows = tableRows + `
@@ -61,14 +49,6 @@ new Vue({
       }
 
       document.getElementById('table_body').innerHTML = tableRows;
-
-      console.log(this.data_size, this.mtu, this.header_length);
-      console.log(payloadOffsetOctets);
-      console.log(payloadOffsetBytes);
-      console.log(fragmentNum);
-      fragments.forEach((item, i) => {
-        console.log(item);
-      });
     },
     checkInput: function() {
       if (!this.data_size || !this.mtu || !this.header_length) {
